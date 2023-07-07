c5891e8f0a4912cd5d56b52704ba541ec4a20133
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-07-07 15:19:03 +0300

    Recreate each with index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,13 +1,19 @@
+#require 'pry-byebug'
 module Enumerable
   # Your code goes here
-  def my_all(pattern = nil)
-  
+  def my_each_with_index
+   return to_enum{:my_each_with_index} unless block_given?
 
+   i = 0
+   while i < self.length do 
+    yield(self[i],i)
+    i += 1
+   end
+   self
   end
 
-
-
 end
+
 
 # You will first have to define my_each
 # on the Array class. Methods defined in
